78d00076b0579c59b44efe96de200fb59474fd1c
Fabricio Soares <fabriciosoares@live.com>
2020-04-23 12:59:57 -0300

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Fabrício Soares on 23/04/20.
+//  Copyright © 2020 Fabrício Soares. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
